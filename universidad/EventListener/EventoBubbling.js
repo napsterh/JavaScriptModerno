b6575756765c2dd85b41b7a560ba1f9de4c1f08b
Napster <rome.1205h@gmail.com>
2020-05-04 17:01:49 -0500

    agregando stopPropagation para evitar el event blobbling

--- universidad/EventListener/EventoBubbling.js
+++ universidad/EventListener/EventoBubbling.js
@@ -2,3 +2,21 @@
 *****************************             EVENT BUBBLING                   ******************
 *************************************************************************************************/
 
+const card = document.querySelector('.card');
+const infoCurso = document.querySelector('.info-card');
+const agregarCarrito = document.querySelector('.agregar-carrito');
+
+card.addEventListener('click', function(e){
+    console.log('click en card');
+    e.stopPropagation(); // DETENER EL EVENT BLOBBLING
+});
+
+infoCurso.addEventListener('click', function(e){
+    console.log('Click en info curso');
+    e.stopPropagation();
+});
+
+agregarCarrito.addEventListener('click', function(e){
+    console.log('Click en agregar carrito');
+    e.stopPropagation();
+});
