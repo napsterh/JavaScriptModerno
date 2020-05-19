55b4279ea3ac33c07f4a06f3c43bbe2a7b70fa22
Napster <rome.1205h@gmail.com>
2020-05-19 11:40:57 -0500

    heredando una clase

--- Clases/app.js
+++ Clases/app.js
@@ -1,3 +1,4 @@
+/*
 class Clientes {
     constructor(nombre, apellido, saldo){
         this.nombresito = nombre;
@@ -38,3 +39,41 @@ maria.retirarSaldo(4000);
 
 
 console.log( Clientes.bienvenido() );
+
+*/
+
+class Clientes {
+    constructor(nombre, saldo){
+        this.nombresito = nombre;
+        this.saldito = saldo;
+    }
+
+    imprimirSaldo(){
+        return ` Hola ${this.nombresito}, tu saldo es de ${this.saldito} `;
+    }
+
+    //Atributo estaticos no necesitan instanciarse
+    static bienvenido() {
+        return `Bienvenido al cajero`;
+    }
+
+
+}
+
+const cliente1 = new Clientes('Homer', 100);
+
+class Empresa extends Clientes {
+    constructor(nombre, saldo, telefono, tipo){
+        //va hacia el conctructor padre
+        super(nombre, saldo);
+        //no existen el constructor padre
+        this.telefonito = telefono;
+        this.tipito = tipo;
+    }
+}
+
+const empresa = new Empresa('empresa1', 1000, 1238461864, 'Construccion');
+
+console.log(empresa);
+console.log(empresa.imprimirSaldo());
+console.log(cliente1.imprimirSaldo());
