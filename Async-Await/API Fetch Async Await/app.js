ad55fc9f8a95d7bee2ed367e2938712580c7c03b
Napster <rome.1205h@gmail.com>
2020-07-27 21:38:47 -0500

    ejemplo de async await

---
+++ Async-Await/API Fetch Async Await/app.js
@@ -0,0 +1,16 @@
+
+
+async function leerTodos(){
+    //esperarla respuesta
+    const respuesta = await fetch
+    ('https://jsonplaceholder.typicode.com/todos');
+
+    //Procede cuando la respuesta este hecha
+    const datos = await respuesta.json();
+
+    return datos;
+}
+
+leerTodos()
+    .then( usuarios => console.log(usuarios));
+
