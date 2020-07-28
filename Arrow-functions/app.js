99fabd1a498fab0184d5089bb21477a49e0f53b9
Napster <rome.1205h@gmail.com>
2020-07-27 20:00:20 -0500

    add arrow functions

---
+++ Arrow-functions/app.js
@@ -0,0 +1,43 @@
+
+let aprendiendo;
+
+
+aprendiendo = function(){
+    console.log('Aprendiendo JavaScript');
+}
+
+aprendiendo = () => {
+    console.log('Aprendiendo JavaScript con arrow Functions');
+}
+
+//Una funcion de una sola linea no requiere  llave
+aprendiendo = () => console.log('funcion en una sola linea');
+
+//Retorna un objeto
+aprendiendo = () => ({ aprendiendo: 'objeto a mostrar' });
+
+//Pasar 1 parametro
+aprendiendo = tecnologia => console.log(`Aprendiendo ${tecnologia}`);
+
+//Pasar 2 parametros
+aprendiendo = (tec1,tec2) => console.log(`Aprendiendo ${tec1} y ${tec2}`);
+
+
+const productos = ['disco', 'camisa', 'guitarra'];
+
+/*const letrasProducto = productos.map(function(producto) {
+    return producto.length;
+});
+
+const letrasProducto = productos.map(producto => {
+    return producto.length;
+});
+const letrasProducto = productos.map(producto => producto.length)
+
+
+productos.forEach(producto =>  {
+    console.log(producto);
+})
+*/
+productos.forEach(producto =>  console.log(producto));
+    
