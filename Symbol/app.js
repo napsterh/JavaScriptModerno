146c8263a1bb02878994d2996efe3b8dc2585a31
Napster <rome.1205h@gmail.com>
2020-08-24 08:25:29 -0500

    symbol

---
+++ Symbol/app.js
@@ -0,0 +1,10 @@
+//Symbol
+
+let nombre = Symbol();
+let apelliido = Symbol();
+
+//crear una persona
+let persona = {}
+persona.nombre = 'Juan';
+
+console.log(persona)
---
+++ Symbol/dist/app.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Symbol
+var nombre = Symbol();
+var apelliido = Symbol(); //crear una persona
+
+var persona = {};
+persona.nombre = 'Juan';
+console.log(persona);
